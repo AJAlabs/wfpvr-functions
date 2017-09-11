30f06536891d48d4c14ae1c3e5066a6598c2bdfc
AJ Acevedo <aj@ajalabs.com>
2017-09-11 14:45:24 -0400

    Updated Google Analytics section
    v0.2

--- wfpvr-functions.php
+++ wfpvr-functions.php
@@ -2,10 +2,10 @@
 /**
  * Plugin Name: WFPVR Functions
  * Plugin URI: https://github.com/AJAlabs/wfpvr_functions
- * Description: Custom WordPress functions.php plugin for Westchester FPV Racing
+ * Description: Custom WordPress functions.php plugin for Westchester FPV Racing.
  * Author: AJ Acevedo
  * Author URI: http://AJAlabs.com
- * Version: 0.1
+ * Version: 0.2
  * License: GPL2
  *
  * @package wfpvr-maintenance-mode
@@ -20,11 +20,20 @@ remove_action('wp_head', 'wp_generator');
 // Remove Windows Live Writer meta from head
 remove_action('wp_head', 'wlwmanifest_link');
 
-// Adds Google Analytics to the footer
-// Insert your code between the Begins and Ends block
-add_action('wp_footer', 'add_google_analytics');
-  function add_google_analytics() { ?>
-<!-- Google Analytics: Begins-->
-<!-- Google Analytics: Ends-->
+// Adds Google Analytics
+function wfpvr_google_analytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-106251419-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 <?php }
 
+add_action( 'wp_head', 'wfpvr_google_analytics', 10 );
+
+?>
